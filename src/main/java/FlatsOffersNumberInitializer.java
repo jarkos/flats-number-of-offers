a6ebc06dfdc3d7f307e236fcd6269b1e222bd9e4
Jarek Kostrzewa <213469@edu.p.lodz.pl>
2020-01-27 21:51:42 +0100

    Fetching rent rooms offers number from olx and otodom

--- src/main/java/FlatsOffersNumberInitializer.java
+++ src/main/java/FlatsOffersNumberInitializer.java
@@ -2,44 +2,29 @@ import lombok.extern.log4j.Log4j2;
 
 import java.time.LocalDate;
 import java.util.ResourceBundle;
+import java.util.Set;
 
 @Log4j2
 public class FlatsOffersNumberInitializer {
 
     private static ResourceBundle rb = ResourceBundle.getBundle("app");
     private static WebsiteDataFetcher websiteDataFetcher = new WebsiteDataFetcher();
+    private static Set<String> bigCities = Set.of("warszawa", "krakow", "lodz", "poznan", "gdansk", "wroclaw", "szczecin",
+            "katowice");
 
     public static void main(String[] args) {
         if (Boolean.valueOf(rb.getString("aggregation.data.enabled"))) {
-            String olxFlatsGenericUrl= rb.getString("olx.real.estate.flats.url");
-            String otodomFlatsSellGenericUrl= rb.getString("otodom.real.estate.flats.url");
-
-            websiteDataFetcher.getOlxOffersData(olxFlatsGenericUrl, "lodz");
-            websiteDataFetcher.getOtodomOffersData(otodomFlatsSellGenericUrl, "lodz");
-            websiteDataFetcher.getOlxOffersData(olxFlatsGenericUrl, "warszawa");
-            websiteDataFetcher.getOtodomOffersData(otodomFlatsSellGenericUrl, "warszawa");
-            websiteDataFetcher.getOlxOffersData(olxFlatsGenericUrl, "poznan");
-            websiteDataFetcher.getOtodomOffersData(otodomFlatsSellGenericUrl, "poznan");
-            websiteDataFetcher.getOlxOffersData(olxFlatsGenericUrl, "krakow");
-            websiteDataFetcher.getOtodomOffersData(otodomFlatsSellGenericUrl, "krakow");
-            websiteDataFetcher.getOlxOffersData(olxFlatsGenericUrl, "gdansk");
-            websiteDataFetcher.getOtodomOffersData(otodomFlatsSellGenericUrl, "gdansk");
-            websiteDataFetcher.getOlxOffersData(olxFlatsGenericUrl, "wroclaw");
-            websiteDataFetcher.getOtodomOffersData(otodomFlatsSellGenericUrl, "wroclaw");
-            websiteDataFetcher.getOlxOffersData(olxFlatsGenericUrl, "szczecin");
-            websiteDataFetcher.getOtodomOffersData(otodomFlatsSellGenericUrl, "szczecin");
-            websiteDataFetcher.getOlxOffersData(olxFlatsGenericUrl, "bydgoszcz");
-            websiteDataFetcher.getOtodomOffersData(otodomFlatsSellGenericUrl, "bydgoszcz");
-            websiteDataFetcher.getOlxOffersData(olxFlatsGenericUrl, "lublin");
-            websiteDataFetcher.getOtodomOffersData(otodomFlatsSellGenericUrl, "lublin");
-            websiteDataFetcher.getOlxOffersData(olxFlatsGenericUrl, "katowice");
-            websiteDataFetcher.getOtodomOffersData(otodomFlatsSellGenericUrl, "katowice");
-
-
-
-
+            String olxFlatsOffersGenericUrl = rb.getString("olx.real.estate.flats.url");
+            String olxRoomsOffersGenericUrl = rb.getString("olx.real.estate.rooms.url");
+            String otodomFlatsOffersGenericUrl = rb.getString("otodom.real.estate.flats.url");
+            String otodomRoomsOffersGenericUrl = rb.getString("otodom.real.estate.rooms.url");
+            bigCities.forEach(city -> {
+                websiteDataFetcher.getOlxFlatsOffersData(olxFlatsOffersGenericUrl, city);
+                websiteDataFetcher.getOtodomFlatsOffersData(otodomFlatsOffersGenericUrl, city);
+                websiteDataFetcher.getOlxRoomsOffersData(olxRoomsOffersGenericUrl, city);
+                websiteDataFetcher.getOtodomRoomsOffersData(otodomRoomsOffersGenericUrl, city);
+            });
         }
         log.warn("Finished apartments stats fetch for: " + LocalDate.now());
     }
-
 }
--- src/main/java/WebsiteDataFetcher.java
+++ src/main/java/WebsiteDataFetcher.java
@@ -3,17 +3,18 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 import java.io.IOException;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Log4j2
 class WebsiteDataFetcher {
 
-    void getOlxOffersData(String url, String cityId) {
+    void getOlxFlatsOffersData(String url, String cityId) {
         try {
             Document doc = Jsoup.connect(url + cityId).get();
             String rentOffers = doc.getElementsByClass("counter nowrap").first().childNode(0).toString();
             String sellOffers = doc.getElementsByClass("counter nowrap").get(3).childNode(0).toString();
-            log.info("Rent " + cityId + " " + rentOffers);
-            log.info("Sell " + cityId + " " + sellOffers);
+            log.info("Rent flat" + cityId + " " + rentOffers);
+            log.info("Sell flat" + cityId + " " + sellOffers);
         } catch (IOException ioe) {
             log.info("IOException happened");
             ioe.printStackTrace();
@@ -23,7 +24,28 @@ class WebsiteDataFetcher {
         }
     }
 
-    void getOtodomOffersData(String url, String cityId) {
+    void getOlxRoomsOffersData(String url, String cityId) {
+        try {
+            Document doc = Jsoup.connect(url + cityId).get();
+            AtomicInteger sum = new AtomicInteger();
+            doc.getElementsByClass("locationlinks margintop10").first()
+                    .childNodes().stream().filter(node -> !node.toString().equals(" ")).forEach(node -> node.childNodes()
+                    .stream().filter(subnode -> !subnode.toString().equals(" ")).forEach(subnode -> {
+                        var resultOfDistrict = subnode.childNode(1).toString()
+                                .replace("&nbsp;(", "").replace(")", "");
+                        sum.addAndGet(Integer.valueOf(resultOfDistrict));
+                    }));
+            log.info("ROOMS " + cityId + " " + sum);
+        } catch (IOException ioe) {
+            log.info("IOException happened");
+            ioe.printStackTrace();
+        } catch (Exception e) {
+            log.info("Some exception happened!");
+            e.printStackTrace();
+        }
+    }
+
+    void getOtodomFlatsOffersData(String url, String cityId) {
         try {
             var sellUrl = url.replace("PLACEHOLDER_TO_REPLACE", "sprzedaz");
             var rentUrl = url.replace("PLACEHOLDER_TO_REPLACE", "wynajem");
@@ -33,8 +55,23 @@ class WebsiteDataFetcher {
                     .childNode(1).childNode(0).toString();
             String rentOffers = docRent.getElementsByClass("offers-index pull-left text-nowrap").first()
                     .childNode(1).childNode(0).toString();
-            log.info("Rent " + cityId + " " + rentOffers);
-            log.info("Sell " + cityId + " " + sellOffers);
+            log.info("Rent flat otodom " + cityId + rentOffers);
+            log.info("Sell flat otodom " + cityId + sellOffers);
+        } catch (IOException ioe) {
+            log.info("IOException happened");
+            ioe.printStackTrace();
+        } catch (Exception e) {
+            log.info("Some exception happened!");
+            e.printStackTrace();
+        }
+    }
+
+    void getOtodomRoomsOffersData(String url, String cityId) {
+        try {
+            Document docRent = Jsoup.connect(url + cityId).get();
+            String rentOffers = docRent.getElementsByClass("offers-index pull-left text-nowrap").first()
+                    .childNode(1).childNode(0).toString();
+            log.info("Rent room otodom " + cityId + " " + rentOffers);
         } catch (IOException ioe) {
             log.info("IOException happened");
             ioe.printStackTrace();
