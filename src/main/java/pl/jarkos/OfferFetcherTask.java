c0c206ad4ce53d357acfb59965a6f67a3d99ab65
Jarek Kostrzewa <213469@edu.p.lodz.pl>
2021-04-04 13:42:10 +0200

    local IP address print + backup H2 DB 04.04.21

--- src/main/java/pl/jarkos/OfferFetcherTask.java
+++ src/main/java/pl/jarkos/OfferFetcherTask.java
@@ -2,16 +2,18 @@ package pl.jarkos;
 
 import lombok.extern.log4j.Log4j2;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.time.LocalDate;
-import java.util.List;
 import java.util.ResourceBundle;
+import java.util.Set;
 
 @Log4j2
 public class OfferFetcherTask implements Runnable {
 
     private static ResourceBundle rb = ResourceBundle.getBundle("app");
     private static WebsiteDataFetcher websiteDataFetcher = new WebsiteDataFetcher();
-    static final List<String> cities = List.of("warszawa", "krakow", "lodz", "poznan", "gdansk", "wroclaw", "szczecin",
+    static final Set<String> cities = Set.of("warszawa", "krakow", "lodz", "poznan", "gdansk", "wroclaw", "szczecin",
             "katowice");
 
     @Override
@@ -27,5 +29,11 @@ public class OfferFetcherTask implements Runnable {
             websiteDataFetcher.getOtodomRoomsOffersData(otodomRoomsOffersGenericUrl, city);
         });
         log.info("Finished apartments stats fetch for: " + LocalDate.now());
+        try {
+            InetAddress localhost = InetAddress.getLocalHost();
+            log.info("System IP Address : " + (localhost.getHostAddress()).trim());
+        } catch (UnknownHostException e) {
+            log.error(e);
+        }
     }
 }
